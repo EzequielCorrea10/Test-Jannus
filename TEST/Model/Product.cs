4226bbcdf51cb35101387edfa0937a87c0f5ecc7
Whacky <ezequielcorrea10@gmail.com>
2024-03-28 18:18:34 -0300

    Set Up

---
+++ TEST/Model/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST.Model
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public int ProductTypeId { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+        public Product(string name, float price, int typeId )
+        {
+            this.Name = name;
+            this.Price = price;
+            this.ProductTypeId = typeId;
+
+        }
+    }
+}
---
+++ TEST/Services/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEST.Model;
+
+namespace TEST.Services
+{
+    public class ProductService
+    {
+        public void CreateProduct(string name, float price, int typeId)
+        {
+            var product = new Product(name, price, typeId);
+
+        }
+
+    }
+}
