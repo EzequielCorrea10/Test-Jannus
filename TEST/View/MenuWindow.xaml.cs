3b404c2f7d9baa782cc3f2a99a52547335d39035
Whacky <ezequielcorrea10@gmail.com>
2024-04-01 23:19:31 -0300

    MainView - ProdcutView Added

---
+++ TEST/View/MenuWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TEST
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+
+        private void StockRep_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            ProductView win2 = new ProductView();
+            win2.Show();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Stock_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TEST/View/ProductView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Objects;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TEST
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class ProductView : Window
+    {
+        public ProductView()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            this.Close();        
+        }
+
+        private void ListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
